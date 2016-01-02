59d00468b3fbcfbbe1c64a25ee126726967a2261
kozakerro <kozakiewiczmichal1@gmail.com>
2016-01-02 22:01:53 +0100

    Wyszukiwanie - GUI

--- public/modules/menu-ctrl.js
+++ public/modules/menu-ctrl.js
@@ -1,26 +1,72 @@
 define(['./module'], function (controllers) {
     'use strict';
+    
+    controllers.factory('SearchService', function($http) {
+        var baseUrl = "http://localhost:3000";
+        return {
 
-    controllers.controller('MenuCtrl', ['AuthenticationService', 'UserService', '$cookies', '$scope', '$state',
-        function (AuthenticationService, UserService, $cookies, $scope, $state) {
+            searchUsers: function(searchString, token) {
+                var users = $http.post(baseUrl + '/user/search/', {
+                    'searchString': searchString,
+                    headers: {
+                        'x-access-token': token
+                    }
+                });
+                return users;
+            },
+            
+            searchTrips: function(searchString, token) {
+                var trips = $http.post(baseUrl + '/trip/search/', {
+                    'searchString': searchString,
+                    headers: {
+                        'x-access-token': token
+                    }
+                });
+                return trips;
+            }
+
+        }
+    });
+
+    controllers.controller('MenuCtrl', ['AuthenticationService', 'UserService', 'SearchService', '$cookies', '$scope', '$state',
+        function (AuthenticationService, UserService, SearchService, $cookies, $scope, $state) {
         var mCtrl = this;
+        var token = $cookies.get('token');
+        
+        mCtrl.searchString = "";
+        mCtrl.foundUsers = null;
+        mCtrl.foundTrips = null;
 
         mCtrl.getUser = function(){return JSON.parse($cookies.get('user'));}
 
-        if(!(typeof $cookies.get('token') === 'undefined')){
+        if(!(typeof(token) === 'undefined')){
             AuthenticationService.setLoggedInFlag(true);
             mCtrl.user = mCtrl.getUser();
             AuthenticationService.setUser(mCtrl.user);
         }
-
-
-
+        
         $scope.$watch(function(){return AuthenticationService.getLoggedInFlag()},
             function(value){
                 mCtrl.isLogged = value;
             }
         );
 
+        $scope.$watch('mCtrl.searchString', function() {
+            mCtrl.foundUsers = null;
+            mCtrl.foundTrips = null;
+            
+            if (mCtrl.isLogged) {
+                SearchService.searchUsers(mCtrl.searchString, token).success(function(data){
+                    mCtrl.foundUsers = data;
+                });
+                
+                SearchService.searchTrips(mCtrl.searchString, token).success(function(data){
+                    mCtrl.foundTrips = data;
+                });
+              
+            }
+            
+        });
 
         mCtrl.logout = function () {
             UserService.logOut();
